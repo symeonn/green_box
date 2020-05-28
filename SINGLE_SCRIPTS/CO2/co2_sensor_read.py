0b5e5041de08db13e96c2119f4ed02de20a98516
mario <symeonn@gmail.com>
2020-05-28 11:53:13 +0200

    +IDE adaptation

--- SINGLE_SCRIPTS/CO2/co2_sensor_read.py
+++ SINGLE_SCRIPTS/CO2/co2_sensor_read.py
@@ -4,8 +4,8 @@ from grove.i2c import Bus
 
 sgp30 = seeed_sgp30.grove_sgp30(Bus())
 while True:
-  data = sgp30.read_measurements()
-  co2_eq_ppm, tvoc_ppb = data.data
-  print("\r  tVOC = {} ppb CO2eq = {}  ".format(
-                               tvoc_ppb, co2_eq_ppm))
-  time.sleep(0.5)
+    data = sgp30.read_measurements()
+    co2_eq_ppm, tvoc_ppb = data.data
+    print("\r  tVOC = {} ppb CO2eq = {}  ".format(
+        tvoc_ppb, co2_eq_ppm))
+    time.sleep(0.5)
--- SINGLE_SCRIPTS/humidity_temperature/humidity_temperature_sensor_read.sh
+++ SINGLE_SCRIPTS/humidity_temperature/humidity_temperature_sensor_read.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pwd
-python ~/green_box/humidity_temperature/examples/AdafruitDHT.py 11 17
+python ~/green_box/humidity_temperature/libs/examples/AdafruitDHT.py 11 17
 
