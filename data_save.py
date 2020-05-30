0f5aa3443b1698206a4d72b3273a0920d1b8ac73
mario <symeonn@gmail.com>
2020-05-30 12:31:03 +0200

    +generate plot to html ~temperature control logs

--- data_save.py
+++ data_save.py
@@ -5,71 +5,71 @@ import co2_data_collect
 import dht_data_collect
 import water_data_collect
 import time
+import pandas as pd
+import plotly.express as px
 
 print("init data save")
 
-syncHours = [10,22]
+syncHours = [10, 22]
 
-fileName = 'grow_data.csv'
 fileFolder = '/home/pi/gb_CSV/'
+csv_file_name = 'grow_data.csv'
+html_file_name = 'grow_data_plot.html'
+
 
 def proceed():
+    voc, co2 = co2_data_collect.getData()
+    print("VOC: {} :: CO2: {} ".format(voc, co2))
 
-	voc, co2 = co2_data_collect.getData()
-	print("VOC: {} :: CO2: {} ".format(voc,co2))
+    humidity, temperature = dht_data_collect.getData()
+    print("Humidity: {} :: Temperature: {} ".format(humidity, temperature))
 
-	humidity, temperature = dht_data_collect.getData()
-	print("Humidity: {} :: Temperature: {} ".format(humidity, temperature))
+    water_temperature = water_data_collect.getWaterTemperatureData()
+    print("Water temperature: {} ".format(water_temperature))
 
-	waterTemperature = water_data_collect.getWaterTemperatureData()
-	print("Water temperature: {} ".format(waterTemperature))
-
-	writeData([voc, co2, humidity, temperature, waterTemperature])
-
-def writeData(readings): #voc, co2, humidity, temperature, waterTemperature
-
-	print("Writing data to CSV...")
-
-        now = datetime.now() # current date and time
-        #print(now)
-        #fileTimestamp = now.strftime("%Y-%m-%d")
-	#print(fileTimestamp)
-	#print(fileTimestamp + fileNamePostfix)
-
-#	timestamp = (now - datetime(1970, 1, 1)).total_seconds()
-
-#	print(readings)
-	readings.insert(0, now.strftime("%Y-%m-%d %H:%M:%S"))
-	readings.insert(6, -1.0)
-	readings.insert(7, -1.0)
-
-#	print(readings)
-
-#	for i in readings :
-#    		print(i)
-	#readings = readings.insert(0, now)
-
-	fileFullPath = fileFolder+fileName
+    write_data([voc, co2, humidity, temperature, water_temperature])
 
 
-	with open(fileFullPath, 'a') as file:
+def write_data(readings):  # voc, co2, humidity, temperature, waterTemperature
 
-		writer = csv.writer(file)
+    print("Writing data to CSV...")
 
-                if os.stat(fileFullPath).st_size == 0:
-	                writer.writerow(["datetime","VOC","CO2","humidity","temperature","water_temperature", "pH", "EC"])
+    current_datetime = datetime.now()
 
-		writer.writerow(readings)
+    readings.insert(0, current_datetime.strftime("%Y-%m-%d %H:%M:%S"))
+    readings.insert(6, -1.0)
+    readings.insert(7, -1.0)
 
+    file_full_path = fileFolder + csv_file_name
+
+    with open(file_full_path, 'a') as file:
+        writer = csv.writer(file)
+
+        if os.stat(file_full_path).st_size == 0:
+            writer.writerow(["datetime", "VOC", "CO2", "humidity", "temperature", "water_temperature", "pH", "EC"])
+
+        writer.writerow(readings)
+
+    write_html_file(file_full_path)
+
+
+def write_html_file(file_full_path=None):
+    df = pd.read_csv(file_full_path)
+    html_file_path: str = fileFolder + html_file_name
+    fig = px.line(df, x='datetime', y=['CO2', 'VOC', 'humidity', 'temperature', 'water_temperature', 'pH', 'EC'],
+                  title='Apple Share Prices over time (2014)')
+    with open(html_file_path, 'w') as file:
+        html = fig.to_html()
+        file.write(html)
 
 
 while True:
 
-	now = datetime.now()
+    now = datetime.now()
 
-	if now.hour in syncHours:
-		print("Getting sensors readings...")
-		proceed()
-		print("Data saving done.")
+    if now.hour in syncHours:
+        print("Getting sensors readings...")
+        proceed()
+        print("Data saving done.")
 
-	time.sleep(55 * 60) #55m
+    time.sleep(55 * 60)  # 55m
--- temperature_control.py
+++ temperature_control.py
@@ -5,39 +5,35 @@ import time
 highTemperatureLevel = 22
 lowTemperatureLevel = 20
 
-
 # relay IN1, GPIO 4 for FAN
 RELAY_GPIO_PIN = 4
 
 
 def init():
-	GPIO.setwarnings(False)
-	print("init temperature control")
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(RELAY_GPIO_PIN, GPIO.OUT) # GPIO Assign mode
-        time.sleep(1)
+    GPIO.setwarnings(False)
+    print("init temperature control")
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(RELAY_GPIO_PIN, GPIO.OUT)  # GPIO Assign mode
+    time.sleep(1)
 
 
-def startMonitoring():
+def start_monitoring():
+    while True:
 
-	while True:
+        humidity, temperature = dht_data_collect.getData()
 
-		humidity, temperature = dht_data_collect.getData()
+        if temperature > highTemperatureLevel:
+            GPIO.output(RELAY_GPIO_PIN, GPIO.LOW)  # on
+            print(f'Temperature: {temperature} *C - FAN on')
+        else:
+            GPIO.output(RELAY_GPIO_PIN, GPIO.HIGH)  # off
+            print(f'Temperature: {temperature} *C - FAN off')
 
-		print("Temperature: {} *C".format(temperature))
-
-		if temperature > highTemperatureLevel:
-		        GPIO.output(RELAY_GPIO_PIN, GPIO.LOW) # on
-			print("FAN on")
-		else:
-		        GPIO.output(RELAY_GPIO_PIN, GPIO.HIGH) # off
-	                print("FAN off")
-
-#		print("watinig...")
-		time.sleep(120) #2m
+        # print("watinig...")
+        time.sleep(120)  # 2m
 
 
 init()
 
-startMonitoring()
-#fan_control.stopFan()
+start_monitoring()
+# fan_control.stopFan()
