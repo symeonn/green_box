f7d98c0bb297735cd1fdedc98cdd196db3cecc76
symeonn <symeonn@gmail.com>
2020-06-22 11:02:36 +0200

    Change settings for air pump (#19)

--- settings.py
+++ settings.py
@@ -2,10 +2,10 @@ fan_enable_temperature_limit_celsius = 21
 fan_monitoring_interval_seconds = 300  # 5m
 
 air_pump_on_interval_night_seconds = 3600  # 1h
-air_pump_off_interval_night_seconds = 900  # 15m
+air_pump_off_interval_night_seconds = 300  # 5m
 air_pump_on_interval_day_seconds = 1800  # 30m
 air_pump_off_interval_day_seconds = 900  # 15m
-air_pump_night_hour_begin = 22
+air_pump_night_hour_begin = 21
 air_pump_night_hour_end = 6
 
 data_save_delay_seconds = 3600  # 1h
