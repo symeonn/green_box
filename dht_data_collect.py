0da1a579205315ed553c5469153233ce26acbab5
mario <symeonn@gmail.com>
2020-06-29 22:31:27 +0200

    fix error with NaN for humidity, temperature

--- dht_data_collect.py
+++ dht_data_collect.py
@@ -9,14 +9,30 @@ def get_single_read_data():
 
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     # print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+
+    try:
+        float(humidity)
+    except ValueError:
+        print("humidity Not a float")
+        humidity = 0.0
+
+    try:
+        float(temperature)
+    except ValueError:
+        print("temperature Not a float")
+        temperature = 0.0
+
     return humidity, temperature
 
 
-def get_read_data(read_count=5):
+def get_read_data(read_count=3):
     sample_array = numpy.zeros((read_count, 2))
 
     for read_number in range(read_count):
-        sample_array[read_number] = get_single_read_data()
+        humidity, temperature = get_single_read_data()
+
+        if humidity != 0.0 and temperature != 0.0:
+            sample_array[read_number] = humidity, temperature
         # print('H={0:0.1f}*  T={1:0.1f}%'.format(sample_array[read_number][0], sample_array[read_number][1]))
         time.sleep(2)
 
