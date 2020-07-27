176644e17c3853316e6512d9c63ea935d2f39caa
symeonn <symeonn@gmail.com>
2020-07-27 10:55:16 +0200

    Python logging and error handling for air_pump_control.py #26 (#27)

--- air_pump_control.py
+++ air_pump_control.py
@@ -2,44 +2,57 @@ import RPi.GPIO as GPIO
 import time
 from datetime import datetime
 import settings
-
-print("init air pump control")
-
-GPIO.setwarnings(False)
-
-GPIO.setmode(GPIO.BCM)
+import file_logger
+import os
 
 # relay IN2, GPIO 27 for air pump
 RELAY_GPIO_PIN = 27
+logs_name = 'air_pump_control.py'
+file_logger.init(logs_name)
 
-GPIO.setup(RELAY_GPIO_PIN, GPIO.OUT)  # GPIO Assign mode
-time.sleep(1)
+
+def init():
+    file_logger.info("Init air pump control")
+
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(RELAY_GPIO_PIN, GPIO.OUT)  # GPIO Assign mode
+    time.sleep(1)
 
 
 def run_night_hours():
-    print("air pump ON (for {} minutes)".format(settings.air_pump_on_interval_night_seconds / 60))
+    file_logger.info("air pump ON (for {} minutes)".format(settings.air_pump_on_interval_night_seconds / 60))
     GPIO.output(RELAY_GPIO_PIN, GPIO.LOW)  # on
     time.sleep(settings.air_pump_on_interval_night_seconds)
 
-    print("air pump OFF (for {} minutes)".format(settings.air_pump_off_interval_night_seconds / 60))
+    file_logger.info("air pump OFF (for {} minutes)".format(settings.air_pump_off_interval_night_seconds / 60))
     GPIO.output(RELAY_GPIO_PIN, GPIO.HIGH)  # off
     time.sleep(settings.air_pump_off_interval_night_seconds)
 
 
 def run_day_hours():
-    print("air pump ON (for {} minutes)".format(settings.air_pump_on_interval_day_seconds / 60))
+    file_logger.info("air pump ON (for {} minutes)".format(settings.air_pump_on_interval_day_seconds / 60))
     GPIO.output(RELAY_GPIO_PIN, GPIO.LOW)  # on
     time.sleep(settings.air_pump_on_interval_day_seconds)
 
-    print("air pump OFF (for {} minutes)".format(settings.air_pump_off_interval_day_seconds / 60))
+    file_logger.info("air pump OFF (for {} minutes)".format(settings.air_pump_off_interval_day_seconds / 60))
     GPIO.output(RELAY_GPIO_PIN, GPIO.HIGH)  # off
     time.sleep(settings.air_pump_off_interval_day_seconds)
 
 
-while True:
-    now = datetime.now()
+def start():
+    while True:
+        try:
+            now = datetime.now()
 
-    if settings.air_pump_night_hour_begin <= now.hour or now.hour <= settings.air_pump_night_hour_end:
-        run_night_hours()
-    else:
-        run_day_hours()
+            if settings.air_pump_night_hour_begin <= now.hour or now.hour <= settings.air_pump_night_hour_end:
+                run_night_hours()
+            else:
+                run_day_hours()
+        except Exception as e:
+            file_logger.error('Error in: ' + os.path.basename(__file__))
+            file_logger.error(e)
+
+
+init()
+start()
